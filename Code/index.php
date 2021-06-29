cc1654a812e6ec1399e0ff2481b6f6c09a90e252
RomainGRANGE <rgrange699@gmail.com>
2021-06-29 18:03:39 +0200

    problemes desktop img

---
+++ Code/index.php
@@ -0,0 +1,34 @@
+<html lang="fr">
+<head>
+    <link rel="stylesheet" href="style/style.css">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+</head>
+<body>
+    <div class="container">
+        <div class="container__image">
+            <img class="container__image__mobile" src="../stats-preview-card-component-main/images/image-header-mobile.jpg" alt="image">
+            <img class="container__image__desk" src="../stats-preview-card-component-main/images/image-header-desktop.jpg" alt="image">        
+        </div>
+        <div class="container__content">
+            <div class="content__content">
+                <h2 class="container__content__title">Get<span> insights </span>that help your business grow.</h2>
+                <p class="container__content__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur veniam ad beatae, ipsa blanditiis aperiam fuga aspernatur.</p>
+                <div class="container__content__chiffres">
+                    <div class="container__content__chiffres__chiffre">
+                        <span class="container__content__chiffres__chiffre__span">10k+</span>
+                        <p class="container__content__chiffres__chiffre__text">COMPANIES</p>
+                    </div>
+                    <div class="container__content__chiffres__chiffre">
+                        <span class="container__content__chiffres__chiffre__span">314</span>
+                        <p class="container__content__chiffres__chiffre__text">TEMPLATES</p>
+                    </div> 
+                    <div class="container__content__chiffres__chiffre">
+                        <span class="container__content__chiffres__chiffre__span">12M+</span>
+                        <p class="container__content__chiffres__chiffre__text">QUERIES</p>
+                    </div>             
+                </div>
+            </div>
+        </div>    
+    </div>
+</body>
+</html>
